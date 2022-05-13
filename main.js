6874cbbf80f20362547f7f8dca9123c97b51e8e2
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2022-05-13 09:20:04 -0300

    aula 4

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElemntoAudio){
+    document.querySelector(idElemntoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+/*
+let contador = 0;
 
-listaDeTeclas[0].onclick=tocaSomPom;
+while (contador < listaDeTeclas.length){
 
-const listaDeSons = document.querySelectorAll('');
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+    
+    //console.log(idAudio);
+
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
+    contador ++;
+}*/
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+    
+    //console.log(idAudio);
+
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
+}
